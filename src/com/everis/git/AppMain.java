ef4128fb3e228b1c750549013d4e1a2a53e2e9c3
jhermiac <jhermiac@everis.com>
2021-03-25 11:07:25 +0100

    Version 1 - GIT

---
+++ src/com/everis/git/AppMain.java
@@ -0,0 +1,23 @@
+package com.everis.git;
+
+/**
+ * FORMACIÓN DUAL EVERIS - PRIMER DESAFÍO GIT
+ * 
+ * @author jhermiac
+ *
+ */
+
+public class AppMain {
+
+	/**
+	 * Método Principal
+	 * 
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		System.out.println("Hola mundo");
+
+	}
+
+}
