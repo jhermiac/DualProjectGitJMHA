ef876859303589c81850de640c3bd693117b9b72
jhermiac <jhermiac@everis.com>
2021-03-25 11:37:25 +0100

    Version 3 - GIT

--- src/com/everis/git/AppMain.java
+++ src/com/everis/git/AppMain.java
@@ -17,6 +17,8 @@ public class AppMain {
 
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
+		System.out.println("Juanma Hermida Acuña");
+		System.out.println("Sevillano");
 
 	}
 
